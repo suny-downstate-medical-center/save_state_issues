80f4dd8efa956d1b514606e011bc847b08b7df97
craig <kelley92@bu.edu>
2021-06-30 11:45:49 -0400

    mpi test

---
+++ mpi_test.py
@@ -0,0 +1,11 @@
+from mpi4py import MPI
+from neuron import h 
+h.load_file('stdrun.hoc')
+
+pc = h.ParallelContext()
+pcid = pc.id()
+nhost =pc.nhost()
+pc.timeout(0)
+pc.set_maxstep(100)
+
+print("I am " + str(pcid))
